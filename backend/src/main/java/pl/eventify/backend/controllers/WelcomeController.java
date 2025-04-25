b94d78413603e6e8cb002c05488d55e029b76adc
19Pawel97 <19pawel9772@gmail.com>
2025-04-25 22:09:50 +0200

    First controller added to check if backend and frontend are connected and cowork

---
+++ backend/src/main/java/pl/eventify/backend/controllers/WelcomeController.java
@@ -0,0 +1,17 @@
+package pl.eventify.backend.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:5173")  // pozwala frontendowi na zapytania
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to Eventify";
+    }
+}
